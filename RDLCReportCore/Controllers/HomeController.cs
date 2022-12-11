a351a16925b8d903b138c46daecd38ac0446cfa0
kaungsan-dev <kaungsandotcom@gmail.com>
2022-12-11 20:18:01 +0630

    merge

---
+++ RDLCReportCore/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RDLCReportCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDLCReportCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public Report report = new Report();
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            var dt = new DataTable();
+            dt = report.GetReportList();
+
+            string mimeType = "";
+            int extension = 1;
+            var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\ReportExpense.rdlc";
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm1", "RDCL Report");
+            parameters.Add("prm2", DateTime.Now.ToString("dd-MM-yyyy"));
+            parameters.Add("prm3", "Expense Report");
+
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("dsExpense", dt);
+
+            var res = localReport.Execute(RenderType.Pdf, extension, parameters, mimeType);
+            return File(res.MainStream, "application/pdf");
+        }
+    }
+}
---
+++ RDLCReportCore/Models/Report.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDLCReportCore.Models
+{
+    public class Report
+    {
+        string connectionString = "Data Source=localhost;Initial Catalog=CRUDOnly;Integrated Security=SSPI; User ID=madbadmin; password=@dmin123";
+
+        public DataTable GetReportList()
+        {
+            var dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SPReportExpense", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                con.Close();
+            }
+            return dt;
+        }
+    }
+}
